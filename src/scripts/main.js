d4e963b1b3b96ef6078f844b46ed847205e659a1
Oscar-Kristensson <kristenssonomg@gmail.com>
2025-02-01 20:14:20 +0100

    Added the information bar + implemented new scrollbar behavior

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,56 +1,9 @@
 const tempTestOutputElement = document.getElementById("tempTestOutputElement");
 
-// Test code, temporary
-const testPrompt = new ChatPrompt(
-    "Hello World!",
-    "llama3.1"
-)
-
-
-
-// Most likely a function only for testing
-function askPrompt(promptText) {
-    const prompt = new ChatPrompt(
-        promptText,
-        "llama3.2:3b"
-    )
-
-    prompt.addCallback("startedResponding", (prompt) => {
-        tempTestOutputElement.innerText = "";
-        prompt.rawResponse = "";
-    });
-
-
-    prompt.addCallback("streamUpdate", (prompt) => {
-        tempTestOutputElement.innerText = prompt.rawResponse;
-    })
-
-    prompt.addCallback("finished", (prompt) => {
-        console.log(prompt.resultResponse);
-    })
-
-    Ollama.executePrompt(prompt);
-
-
-};
 
 Ollama.initalize()
 
 const chatContainer = document.querySelector(".chatContainer");
-
-function runTest() {
-    console.log("Executing prompt!");
-
-    console.log(Ollama.getLocalModels());
-
-
-    const prompt = new ChatPrompt("What is python", "llama3.2:3b");
-
-    const response = new ChatResponse(chatContainer, prompt);
-
-    Ollama.executePrompt(prompt);
-    
-}
 
 const messageComposeElement = document.querySelector(".messageComposeElement");
 
--- src/scripts/normalChat/chatResponse.js
+++ src/scripts/normalChat/chatResponse.js
@@ -2,6 +2,8 @@
 
 class ChatResponse {
     constructor (parent, prompt) {
+        this.parent = parent;
+
         this.container = document.createElement("div");
         this.container.className = "messageContainer";
         this.container.classList.add("response");
@@ -12,16 +14,40 @@ class ChatResponse {
         this.container.appendChild(this.messageTextContainer);
 
 
+        this.informationBar = document.createElement("div");
+        this.informationBar.className = "informationBar";
+        this.container.appendChild(this.informationBar);
+
+        this.modelDisplayElement = document.createElement("div");
+        this.modelDisplayElement.className = "displayElement";
+        this.modelDisplayElement.classList.add("model");
+        this.modelDisplayElement.innerText = prompt.model;
+        this.informationBar.appendChild(this.modelDisplayElement);
+
+        this.timeDisplayElement = document.createElement("div");
+        this.timeDisplayElement.className = "displayElement";
+        this.timeDisplayElement.classList.add("model");
+        this.timeDisplayElement.innerText = "Running";
+        this.informationBar.appendChild(this.timeDisplayElement);
+        this.informationBar.style.userSelect = "none";
+
         this.prompt = prompt;
 
         this.prompt.addCallback("streamUpdate", () => { this.updateResponse(); });
 
-        parent.appendChild(this.container);       
+        this.prompt.addCallback("finished", () => { this.finishedResponse() });
+
+        this.parent.appendChild(this.container);       
 
     }
+
+    finishedResponse() {
+        this.timeDisplayElement.innerText = String(Math.round(this.prompt.resultResponse.total_duration / 10**6) / 10**3) + "s";
+    };
 
 
     updateResponse(prompt) {
         this.messageTextContainer.innerText = this.prompt.rawResponse;
+        this.parent.scrollTop = this.parent.scrollHeight;
     };
 };
