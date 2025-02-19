419b96ce5cfbc21b465457cf004de8bc275fbaa0
Oscar-Kristensson <kristenssonomg@gmail.com>
2025-02-19 20:46:44 +0100

    Added getLastElement to utils

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -3,12 +3,17 @@
 class _Utils {
     constructor () {
 
-    }
+    };
 
     max(a, b) {
         if (a > b) return a;
         return b;
-    }
+    };
+
+    getLastElement(list) {
+        return list[list.length - 1];
+    };
+
 }
 
 const Utils = new _Utils();
